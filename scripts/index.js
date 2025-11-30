d27b6ec77dee18d41bfdafe9ba5a1e453fea367a
Emmanuel Gutierrez <gutierrezmanny13@gmail.com>
2025-11-30 11:26:10 -0600

    Generate cards dynamically

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,44 @@
 const initialCards = [
   {
     name: "Mountain",
-    link: "https://unsplash.com/photos/green-trees-near-gray-mountain-during-daytime-6SHkCsvgfy0",
+    link: "https://images.unsplash.com/photo-1604644325176-2b8eb8a3ac8c?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Hamster",
-    link: "https://unsplash.com/photos/selective-focus-photography-of-brown-hamster-adK3Vu70DEQ",
+    link: "https://images.unsplash.com/photo-1425082661705-1834bfd09dca?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Pizza",
-    link: "https://unsplash.com/photos/pizza-on-chopping-board-MqT0asuoIcU",
+    link: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=962&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Cathedral",
-    link: "https://unsplash.com/photos/brown-cathedral-during-daytime-d0xjEv-WJQk",
+    link: "https://images.unsplash.com/photo-1511527661048-7fe73d85e9a4?q=80&w=930&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Cabin",
-    link: "https://unsplash.com/photos/wooden-house-near-pine-trees-and-pond-coated-with-snow-during-daytime-OgcJIKRnRC8",
+    link: "https://images.unsplash.com/photo-1510798831971-661eb04b3739?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Movies",
-    link: "https://unsplash.com/photos/a-red-and-white-striped-bag-of-popcorn-next-to-a-red-and-white-striped-bag-djvkPJbH3bE",
+    link: "https://plus.unsplash.com/premium_photo-1710522706751-c2f0c76cc5fd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Bridge",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
   },
 ];
+
+const songTemplate = document.querySelector("#song-template");
+const previewModal = document.querySelector("#modal__preview");
+const previewModalImg = previewModal.querySelector(".modal__preview-img");
+const previewModalExit = previewModal.querySelector(
+  ".modal__preview-exit-button"
+);
+const previewModalCaption = previewModal.querySelector(
+  ".modal__preview-caption"
+);
+
 const editProfile = document.querySelector(".profile__edit-button-layout");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editExitButton = editProfileModal.querySelector(".modal__exit-icon");
@@ -38,6 +53,44 @@ const editProfileForm = editProfileModal.querySelector(".modal__form");
 const newPostLinkInput = newPostModal.querySelector("#image-link");
 const newPostCaptionInput = newPostModal.querySelector("#caption");
 const newPostForm = newPostModal.querySelector(".modal__form");
+const cardList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = songTemplate.content.cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__description");
+  const cardImg = cardElement.querySelector(".card__image");
+  const card = cardElement.querySelector(".card");
+
+  cardTitle.textContent = data.name;
+  cardImg.src = data.link;
+  cardImg.alt = data.name;
+
+  const cardLikeButton = cardElement.querySelector(".card__button");
+  cardLikeButton.addEventListener("click", function () {
+    cardLikeButton.classList.toggle("card__button-liked");
+    cardLikeButton.classList.toggle("card__button");
+  });
+
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+  cardDeleteButton.addEventListener("click", function () {
+    card.remove();
+  });
+
+  const cardImgButton = cardElement.querySelector(".card__img-button");
+
+  cardImgButton.addEventListener("click", function () {
+    previewModalCaption.textContent = cardTitle.textContent;
+    previewModalImg.src = cardImg.src;
+    previewModalImg.alt = cardImg.alt;
+    openModal(previewModal);
+  });
+
+  previewModalExit.addEventListener("click", function () {
+    closeModal(previewModal);
+  });
+
+  return cardElement;
+}
 
 function openModal(open) {
   open.classList.add("modal_is-opened");
@@ -76,13 +129,25 @@ editProfileForm.addEventListener("submit", editProfileSubmitHandler);
 
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
-  console.log(newPostLinkInput.value);
-  console.log(newPostCaptionInput.value);
+
+  const cardLink = newPostModal.querySelector("#image-link");
+  const cardCaption = newPostModal.querySelector("#caption");
+  const cardLinkValue = cardLink.value;
+  const cardCaptionValue = cardCaption.value;
+
+  const newCardData = {
+    name: cardCaptionValue,
+    link: cardLinkValue,
+  };
+
+  const newCard = getCardElement(newCardData);
+  cardList.prepend(newCard);
+
   closeModal(newPostModal);
 }
 
 newPostForm.addEventListener("submit", handleAddCardSubmit);
 
-initialCards.forEach(function (card) {
-  console.log(card.name);
+initialCards.forEach((card) => {
+  cardList.prepend(getCardElement(card));
 });
